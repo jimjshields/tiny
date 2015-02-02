bc412793ba6b7b3f73da3c91d1d766c09a8fbdf8
jimjshields <jim.j.shields@gmail.com>
2015-02-02 14:57:16 -0500

    Added tiny.py; separated into logical sections.

---
+++ tiny.py
@@ -0,0 +1,18 @@
+"""
+tiny.py is a practice project that implements the 
+most basic possible web framework in Python.
+"""
+
+# TODO: WSGI Server
+
+# TODO: HTTP
+
+# TODO: Request class
+
+# TODO: Response class
+
+# TODO: Headers
+
+# TODO: Routing
+
+# TODO: Error handling
