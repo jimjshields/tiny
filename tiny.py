7ad2b5e116fdf2b1a36b2363d968294559fef57a
jimjshields <jim.j.shields@gmail.com>
2015-02-10 13:51:50 -0500

    Reorganized code. Deleted unnecessary comments and function calls.

--- tiny.py
+++ tiny.py
@@ -5,91 +5,6 @@ most basic possible web framework in Python.
 
 # cgi used for form parsing; inspect used for argument counting for routing.
 import cgi, inspect
-# urls used for defining routes. Ultimately this will be removed; the app
-# developer should be doing this on their own.
-# The developer will import Tiny methods and define everything on their own.
-# from urls import *
-
-### Server and run script ###
-
-def wsgiref_server(app, host='', port=8080):
-	"""Implements a WSGIref server and serves continuously.
-	   Can override default host and port if desired."""
-	
-	from wsgiref.simple_server import make_server
-
-	host_pretty = 'localhost' if host == '' else host
-	print 'Starting wsgiref_server at %s:%s' % (host_pretty, port)
-
-	server = make_server(host, port, app)
-	server.serve_forever()
-
-def run_app(app):
-	"""Runs a given app with a WSGIref server.
-	   The server does this by invoking the 'callable' provided by the app.
-	   This is to WSGI specs."""
-
-	print 'wsgiref_server is invoking the WSGI callable object.'
-
-	wsgiref_server(app)
-
-### Request and Response Classes ###
-
-class TinyRequest(object):
-	"""Represents a request object. An empty one is created upon starting the app.
-	   When a user makes a request, it will bind user's request information
-	   (environment, queries, post data) to the request object so that it can
-	   be used elsewhere."""
-
-	def __init__(self, environ):
-		"""Binds the request object to the user's request data."""
-
-		self.environ = environ
-		self.path = self.environ.get('PATH_INFO', '/')
-
-		# Standardizes path slashes.
-		self.path = '/' + self.path if not self.path[0] == '/' else self.path
-
-		# Standardizes request methods.
-		self.method = self.environ.get('REQUEST_METHOD', '').upper()
-
-		self.get_data = None
-		self.post_data = None
-
-		if self.method == 'GET':
-			self.get_data = self.get()
-		elif self.method == 'POST':
-			self.post_data = self.post()
-
-	def get(self):
-		"""Parses the query string of a get request and returns it in 
-		   a dictionary."""
-
-		form = cgi.FieldStorage(fp=self.environ.get('wsgi.input'), environ=self.environ)
-		get_dict = {key: form.getvalue(key) for key in form.keys()}
-		return get_dict
-
-	def post(self):
-		"""Parses the data of a post request and returns it in a dictionary."""
-
-		form = cgi.FieldStorage(fp=self.environ.get('wsgi.input'), environ=self.environ)
-		post_dict = {key: form.getvalue(key) for key in form.keys()}
-		return post_dict
-
-class TinyResponse(object):
-	"""Represents a request object. It is initialized upon starting the app.
-	   When a user makes a request, the app will formulate the response based
-	   on the request data. Tiny will bind that response data to an object of
-	   this class."""
-
-	def __init__(self, body, status='200 OK', headers=[('Content-Type', 'text/html')]):
-		"""Creates a response object that can hold the data for the HTTP response.
-		   Defaults to a '200 OK' response with HTML content but allows user to 
-		   override."""
-		
-		self.body = body
-		self.status = status
-		self.headers = headers
 
 class TinyApp(object):
 	"""Represents an app created by the user.
@@ -166,13 +81,90 @@ class TinyApp(object):
 
 		return self.request_handler(environ, start_response)
 
+### Request and Response Classes ###
+
+class TinyRequest(object):
+	"""Represents a request object. An empty one is created upon starting the app.
+	   When a user makes a request, it will bind user's request information
+	   (environment, queries, post data) to the request object so that it can
+	   be used elsewhere."""
+
+	def __init__(self, environ):
+		"""Binds the request object to the user's request data."""
+
+		self.environ = environ
+		self.path = self.environ.get('PATH_INFO', '/')
+
+		# Standardizes path slashes.
+		self.path = '/' + self.path if not self.path[0] == '/' else self.path
+
+		# Standardizes request methods.
+		self.method = self.environ.get('REQUEST_METHOD', '').upper()
+
+		self.get_data = None
+		self.post_data = None
+
+		if self.method == 'GET':
+			self.get_data = self.get()
+		elif self.method == 'POST':
+			self.post_data = self.post()
+
+	def get(self):
+		"""Parses the query string of a get request and returns it in 
+		   a dictionary."""
+
+		form = cgi.FieldStorage(fp=self.environ.get('wsgi.input'), environ=self.environ)
+		get_dict = {key: form.getvalue(key) for key in form.keys()}
+		return get_dict
+
+	def post(self):
+		"""Parses the data of a post request and returns it in a dictionary."""
+
+		form = cgi.FieldStorage(fp=self.environ.get('wsgi.input'), environ=self.environ)
+		post_dict = {key: form.getvalue(key) for key in form.keys()}
+		return post_dict
+
+class TinyResponse(object):
+	"""Represents a request object. It is initialized upon starting the app.
+	   When a user makes a request, the app will formulate the response based
+	   on the request data. Tiny will bind that response data to an object of
+	   this class."""
+
+	def __init__(self, body, status='200 OK', headers=[('Content-Type', 'text/html')]):
+		"""Creates a response object that can hold the data for the HTTP response.
+		   Defaults to a '200 OK' response with HTML content but allows user to 
+		   override."""
+		
+		self.body = body
+		self.status = status
+		self.headers = headers
+
 
 # TODO: HTTP/WSGI Headers
 
 
 # TODO: Error handling
 
-# Initialize the global request object, which will store any request data.
 
-if __name__ == '__main__':
-	run_app(request_handler)
+### Server and run script ###
+
+def wsgiref_server(app, host='', port=8080):
+	"""Implements a WSGIref server and serves continuously.
+	   Can override default host and port if desired."""
+	
+	from wsgiref.simple_server import make_server
+
+	host_pretty = 'localhost' if host == '' else host
+	print 'Starting wsgiref_server at %s:%s' % (host_pretty, port)
+
+	server = make_server(host, port, app)
+	server.serve_forever()
+
+def run_app(app):
+	"""Runs a given app with a WSGIref server.
+	   The server does this by invoking the 'callable' provided by the app.
+	   This is to WSGI specs."""
+
+	print 'wsgiref_server is invoking the WSGI callable object.'
+
+	wsgiref_server(app)
