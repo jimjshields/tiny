56fd940bbf624165b3d163553f92da31edf2b71b
jimjshields <jim.j.shields@gmail.com>
2015-02-11 11:15:28 -0500

    Added import of tiny module to the init script. Updated version for PyPI.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tiny',
-	  version='0.1.1',
+	  version='0.1.2',
 	  description='A small web framework for learning about web frameworks.',
 	  url='https://github.com/jimjshields/tiny',
 	  author='Jim Shields',
