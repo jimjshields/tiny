fd75b5ec117287b77d337b66a8d15434a69bcea1
jimjshields <jim.j.shields@gmail.com>
2015-02-24 11:32:47 -0500

    Added most tests for TinyApp. Still need request_handler test.

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ class TestTinyApp(unittest.TestCase):
 		"""Starts each test with a new, empty TinyApp object and a handler."""
 
 		self.app = tiny.TinyApp()
+		self.app.set_template_path('tiny/templates')
 
 	def tearDown(self):
 		"""Ends each test by destroying the TinyApp object."""
@@ -35,13 +36,27 @@ class TestTinyApp(unittest.TestCase):
 
 		self.assertEqual(self.app.ROUTES, {'/index': (decorator_handler, ['GET'])})
 
+	def test_request_handler(self):
+		"""Tests that the request handler receives a request and returns an
+		   appropriate response."""
+
+		# Test to go here when best approach is decided.
+
 	def test_set_template_path(self):
 		"""Tests that registering the template path to the app saves it to the app."""
 
-		self.app.set_template_path('/test/path/templates')
-		self.assertEqual(self.app.template_path, '/test/path/templates')
+		self.assertEqual(self.app.template_path, 'tiny/templates')
 
+	def test_render(self):
+		"""Tests that rendering a given template works."""
 
+		template_content = self.app.render('test.html')
+		self.assertEqual(template_content, 'Testing templates')
+
+class TestTinyRequest(unittest.TestCase):
+	"""Base class for testing TinyRequest."""
+
+	
 
 if __name__ == "__main__":
 	unittest.main()
